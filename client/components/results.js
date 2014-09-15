a1ca38f585a6374830dfc44af20b216a64db8d9d
DSRoden <Daniel.S.Roden@gmail.com>
2014-09-14 20:47:38 -0500

    gettin' some more results

---
+++ client/components/results.js
@@ -0,0 +1,10 @@
+(function(){
+  'use strict';
+
+  angular.module('collectivist')
+  .factory('Results', ['$http', function($http){
+
+
+  }]);
+})();
+
---
+++ client/views/results/results.js
@@ -0,0 +1,15 @@
+(function(){
+  'use strict';
+
+  angular.module('collectivist')
+  .controller('ResultsCtrl', ['$scope', 'Results', function($scope, Results){
+
+    //$scope.submitResponses = function(){
+      //Results.submit($scope.responses, $scope.form._id).then(function(response){
+        //$scope.results = response.data.results;
+        //console.log($scope.results);
+      //});
+    //};
+  }]);
+})();
+
